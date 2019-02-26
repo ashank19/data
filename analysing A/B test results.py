7b78b31d03252c74456265ce83d91354ac501067
Arnav Ashank <arnavashank.5@gmail.com>
2019-02-26 15:57:53 +0530

    Changed analysing A/

---
+++ analysing A/B test results.py
@@ -0,0 +1,32 @@
+#Analyzing A/B Test Results
+
+#Table of Contents
+
+#Introduction
+
+#Part I - Probability
+
+#Part II - A/B Test
+
+#Part III - Regression
+
+#Introduction
+
+#A/B tests are very commonly performed by data analysts and data scientists.
+
+#For this project, we will be working to understand the results of an A/B test run by an e-commerce website. Our goal is to work through this notebook to help the company understand if they should implement the new page, keep the old page, or perhaps run the experiment longer to make their decision.
+
+
+#Part I - Probability
+
+#To get started, let's import our libraries.
+
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#Setting the seed
+
+random.seed(42)
