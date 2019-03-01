4c7973a4d7260110d9fe919093b9b31e70adf985
Arnav Ashank <arnavashank.5@gmail.com>
2019-03-01 22:54:51 +0530

    Changed a/b_testing.py

--- a/b_testing.py
+++ a/b_testing.py
@@ -176,3 +176,117 @@ plt.axvline(x=obs_diff,color ='red')
 #P-Value: The probability of observing our statistic or a more extreme statistic from the null hypothesis.
 
 #We could also use a built-in to achieve similar results. Though using the built-in might be easier to code, the above portions are a walkthrough of the ideas that are critical to correctly thinking about statistical significance.Calculate the number of conversions for each page, as well as the number of individuals who received each page. Let n_old and n_new refer the number of rows associated with the old page and new pages, respectively.
+
+import statsmodels.api as sm
+
+convert_old = df2.query('group == "control"')['converted'].mean()
+convert_new = df2.query('group == "treatment"')['converted'].mean()
+n_old = df2.query('landing_page == "old_page"').shape[0]
+n_new = df2.query('landing_page == "new_page"').shape[0]
+
+#Now using stats.proportions_ztest to compute our test statistic and p-value.
+
+z_score, p_val=sm.stats.proportions_ztest([convert_old, convert_new], [n_old, n_new])
+
+#What do the z-score and p-value computed above mean for the conversion rates of the old and new pages? Do they agree with the findings beforehand.
+
+#It indicates that the difference is insignificant.
+
+#Hence null hypothesis cannot be rejected which agree with our findings beforehand.
+
+
+#Part III - A regression approach
+
+#The result we achieved in the A/B test in Part II above can also be achieved by performing regression.
+
+#Type of regression we should be performing in this case.
+
+#Logistic regression because here we are dealing with categorical variables.
+
+df2['intercept']=1
+df2=df2.join(pd.get_dummies(df['landing_page']))
+df2['ab_page']=pd.get_dummies(df2['group'])['treatment']
+df2.head()
+
+#Using statsmodels to instantiate regression model on the two columns you created in above, then fitting the model using the two columns you created beforehand to predict whether or not an individual converts.
+
+results=sm.Logit(df2['converted'],df2[['intercept','ab_page']]).fit()
+
+#Optimization terminated successfully.
+         #Current function value: 0.366118
+         #Iterations 6
+
+#Summary of the model below
+
+results.summary()
+
+#Now, considering other things that might influence whether or not an individual converts.
+
+#We can consider adding new factors such timestamp to decide whether it plays an important role in predicting the results better.
+
+#Time stamp can be fuether divided into categories such as morning, Afternoon, Evening etc.
+
+#A disadvantage of adding new factors is that it will make the results complex further if the new factors are dependable with existing explanatory variables then we need to add more complex and higher order terms to help predict better results.
+
+#Now along with testing if the conversion rate changes for different pages, also adding an effect based on which country a user lives in.
+
+c=pd.read_csv('countries.csv')
+c.head()
+
+df3=df2.merge(c,on='user_id',how='left')
+c.country.unique()
+
+df3[['CA','US','UK']]=pd.get_dummies(df3['country'])
+
+df3=df3.drop(df3['CA'])
+df3.head()
+
+df3['intercept']=1
+
+#Fitting a logisitic regresion model into the data.
+
+log_mod = sm.Logit(df3['converted'], df3[['intercept','ab_page','UK', 'US']])
+results = log_mod.fit()
+results.summary()
+
+#From the result above it is clear that the use of columns is not significant in predicting the conversion rate as depicted by the p-values.
+
+1/np.exp(-0.015),np.exp(0.0408),np.exp(0.0506)
+
+#Interpreting Result:
+
+#For every unit for new_page decrease, convert will be 1.5% more likely to happen, holding all other varible constant.
+
+#For every unit for UK increases, convert is 5.2% more to happen, holding all other varible constant.
+
+#For every unit for US increases, convert is 4.2% more to happen, holding all other varible constant.
+
+#Though we have now looked at the individual factors of country and page on conversion, we would now like to look at an interaction between page and country to see if there significant effects on conversion.
+
+df3['UK_new_page'] = df3['ab_page']* df3['UK']
+df3['US_new_page'] = df3['ab_page']* df3['US']
+logit4 = sm.Logit(df3['converted'], df3[['intercept','ab_page','UK_new_page','US_new_page','UK','US']])
+result4 = logit4.fit()
+result4.summary()
+
+np.exp(result4.params)
+
+#Interpreting the result
+
+#From the above Logit Regression Results, we can see the coefficient of intereaction variable "UK_new_page" and "US_new_page" are different from the coefficient of ab_page itself.
+
+#Also,only intercept's p-value is less than 0.05, which is statistically significant enough for converted rate. Other varable in the summary are not statistically significant.
+
+#Additionally, Z-score for all X variables are not large enough to be significant for predicting converted rate.
+
+#Therefore, the country a user lives is not significant on the converted rate considering the page the user land in.
+
+#For every unit for new_page decreases, convert will be 9.34% more likely to happen, holding all other varible constant.
+
+#Convert is 1.08 times more likely to happen for US and new page users than CA and new page users, holding all other varible constant.
+
+#Convert is 1.04 times more likely to happen for UK and new page users than CA and new page users, holding all other varible constant.
+
+#Convert is 1.18 % more likely to happen for the users in US than CA, holding all other varible constant.
+
+#Convert is 1.76 % more likely to happen for the users in UK than CA, holding all other varible constant.
